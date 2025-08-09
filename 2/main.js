95d25c09ff25a9ef2dda58a8a4d59a20c0a790a4
massa200 <jiangyishiye@gmail.com>
2025-08-09 15:18:58 +0900

    マスを作った

--- 2/main.js
+++ 2/main.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('に');
 const taitoru = document.getElementById('いち');let いち= '赤をよけましょう';taitoru.textContent=いち;
-const sikaku1 = document.getElementById('マス1');sikaku1.textContent='';
+const sikaku1 = document.getElementById('マス1');sikaku1.textContent='';const maru1 = document.getElementById('マル1');maru1.textContent='';
 const sikaku2 = document.getElementById('マス2');sikaku2.textContent='';
 const sikaku3 = document.getElementById('マス3');sikaku3.textContent='';
 const sikaku4 = document.getElementById('マス4');sikaku4.textContent='';
@@ -8,16 +8,26 @@ const sikaku5 = document.getElementById('マス5');sikaku5.textContent='';
 const sikaku6 = document.getElementById('マス6');sikaku6.textContent='';
 const sikaku7 = document.getElementById('マス7');sikaku7.textContent='';
 const sikaku8 = document.getElementById('マス8');sikaku8.textContent='';
+const sikaku9 = document.getElementById('マス9');sikaku9.textContent='';
+const sikaku10 = document.getElementById('マス10');sikaku10.textContent='';
+const sikaku11 = document.getElementById('マス11');sikaku11.textContent='';
+const sikaku12 = document.getElementById('マス12');sikaku12.textContent='';
+const sikaku13 = document.getElementById('マス13');sikaku13.textContent='';
+const sikaku14 = document.getElementById('マス14');sikaku14.textContent='';
+const sikaku15 = document.getElementById('マス15');sikaku15.textContent='';
+const sikaku16 = document.getElementById('マス16');sikaku16.textContent='';
 
 button.onclick=function(){
-    let マス1 = '□';sikaku1.textContent=マス1;
-    let マス2 = '□';sikaku2.textContent=マス2;
-    let マス3 = '□';sikaku3.textContent=マス3;
-    let マス4 = '□';sikaku4.textContent=マス4;
-    let マス5 = '□';sikaku5.textContent=マス5;
-    let マス6 = '□';sikaku6.textContent=マス6;
-    let マス7 = '□';sikaku7.textContent=マス7;
-    let マス8 = '□';sikaku8.textContent=マス8;
+    let マス1 = '□';sikaku1.textContent=マス1;let マス2 = '□';sikaku2.textContent=マス2;
+    let マス3 = '□';sikaku3.textContent=マス3;let マス4 = '□';sikaku4.textContent=マス4;
+    let マス5 = '□';sikaku5.textContent=マス5;let マス6 = '□';sikaku6.textContent=マス6;
+    let マス7 = '□';sikaku7.textContent=マス7;let マス8 = '□';sikaku8.textContent=マス8;
+    let マス9 = '□';sikaku9.textContent=マス9;let マス10 = '□';sikaku10.textContent=マス10;
+    let マス11 = '□';sikaku11.textContent=マス11;let マス12 = '□';sikaku12.textContent=マス12;
+    let マス13 = '□';sikaku13.textContent=マス13;let マス14 = '□';sikaku14.textContent=マス14;
+    let マス15 = '□';sikaku15.textContent=マス15;let マス16 = '□';sikaku16.textContent=マス16;
     taitoru.textContent='';
-button.textContent =''
+    button.textContent =''}
+sikaku1.onclick=function(){
+    sikaku1.textContent='';let マル1 = '□';maru1.textContent=マル1
 }
