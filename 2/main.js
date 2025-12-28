6c8d342e621910b3d072f8af8f853f2b6d3ae6cd
massa200 <jiangyishiye@gmail.com>
2025-12-28 19:31:26 +0900

    マルを作った

--- 2/main.js
+++ 2/main.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('に');
-const taitoru = document.getElementById('いち');let いち= '赤をよけましょう';taitoru.textContent=いち;
+const taitoru = document.getElementById('いち');let いち= '○×ゲーム';taitoru.textContent=いち;
 const sikaku1 = document.getElementById('マス1');sikaku1.textContent='';const maru1 = document.getElementById('マル1');maru1.textContent='';
 const sikaku2 = document.getElementById('マス2');sikaku2.textContent='';
 const sikaku3 = document.getElementById('マス3');sikaku3.textContent='';
@@ -17,17 +17,17 @@ const sikaku14 = document.getElementById('マス14');sikaku14.textContent='';
 const sikaku15 = document.getElementById('マス15');sikaku15.textContent='';
 const sikaku16 = document.getElementById('マス16');sikaku16.textContent='';
 
+const sikakus = document.getElementsByClassName('マス')
+const circles = document.getElementsByClassName('マル')
+sikakus[1]
+
 button.onclick=function(){
-    let マス1 = '□';sikaku1.textContent=マス1;let マス2 = '□';sikaku2.textContent=マス2;
-    let マス3 = '□';sikaku3.textContent=マス3;let マス4 = '□';sikaku4.textContent=マス4;
-    let マス5 = '□';sikaku5.textContent=マス5;let マス6 = '□';sikaku6.textContent=マス6;
-    let マス7 = '□';sikaku7.textContent=マス7;let マス8 = '□';sikaku8.textContent=マス8;
-    let マス9 = '□';sikaku9.textContent=マス9;let マス10 = '□';sikaku10.textContent=マス10;
-    let マス11 = '□';sikaku11.textContent=マス11;let マス12 = '□';sikaku12.textContent=マス12;
-    let マス13 = '□';sikaku13.textContent=マス13;let マス14 = '□';sikaku14.textContent=マス14;
-    let マス15 = '□';sikaku15.textContent=マス15;let マス16 = '□';sikaku16.textContent=マス16;
+    for(let i=0;i<sikakus.length;i++){
+    sikakus[i].textContent='□';}
     taitoru.textContent='';
-    button.textContent =''}
-sikaku1.onclick=function(){
-    sikaku1.textContent='';let マル1 = '□';maru1.textContent=マル1
-}
+    button.textContent ='reset';}
+ for(let i=0;i<sikakus.length;i++){
+sikakus[i].onclick=function(){
+        sikakus[i].textContent='';let マル1 = '○';maru1.textContent=マル1
+        sikakus[i].classList.add("マル")
+        }}
